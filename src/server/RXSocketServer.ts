a38a3e8206d2add8d8205e72b11d63ad0e0c6b8f
Luke265 <n.lukas265@gmail.com>
2025-01-05 14:03:39 +0200

    chore: formatting and removed deprecated toPromise

--- src/server/RXSocketServer.ts
+++ src/server/RXSocketServer.ts
@@ -1,16 +1,14 @@
-import { Subject, Observable, merge } from 'rxjs';
-import { RXSocket } from '../RXSocket';
-import { RXSocketMessage } from '../RXSocketMessage';
-import { RXServerSocketEvent } from './RXServerSocketEvent';
-import { RXServerSocketClient } from './RXSocketServerClient';
-import { RXClientSocketEvent } from '../client/RXClientSocketEvent';
-import { IncomingMessage } from 'http';
-import WebSocket = require('ws');
-import { take } from 'rxjs/operators';
-import { EventName } from '../RXSocketEvent';
+import { Subject, Observable, merge, firstValueFrom } from "rxjs";
+import { RXSocket } from "../RXSocket";
+import { RXSocketMessage } from "../RXSocketMessage";
+import { RXServerSocketEvent } from "./RXServerSocketEvent";
+import { RXServerSocketClient } from "./RXSocketServerClient";
+import { RXClientSocketEvent } from "../client/RXClientSocketEvent";
+import { IncomingMessage } from "http";
+import WebSocket = require("ws");
+import { EventName } from "../RXSocketEvent";
 
 export class RXSocketServer implements RXSocket {
-
     readonly message$: Subject<RXSocketMessage<any, any>> = new Subject();
     readonly connection$: Subject<RXSocket> = new Subject();
     readonly sockets: RXServerSocketClient[] = [];
@@ -23,15 +21,14 @@ export class RXSocketServer implements RXSocket {
     private readonly events: { [name: string]: RXServerSocketEvent<any> } = {};
     private server: WebSocket.Server | null = null;
 
-    constructor(public options: WebSocket.ServerOptions = {}) {
-    }
+    constructor(public options: WebSocket.ServerOptions = {}) {}
 
     listen() {
         if (this.server) {
-            throw new Error('Already started');
+            throw new Error("Already started");
         }
         this.server = new WebSocket.Server(this.options);
-        this.server.on('connection', (webSocket, request) => {
+        this.server.on("connection", (webSocket, request) => {
             const socket = this.wrapSocket(webSocket, request);
             socket.message$.subscribe((message: RXSocketMessage<any, any>) => {
                 const event = this.events[message.event.name];
@@ -57,11 +54,11 @@ export class RXSocketServer implements RXSocket {
             this.sockets.push(socket);
             this.connection$.next(socket);
         });
-        this.server.on('listening', () => (this.listening$ as Subject<void>).next());
-        this.server.on('close', () => (this.close$ as Subject<void>).next());
-        this.server.on('error', (_: WebSocket, error: Error) => (this.error$ as Subject<any>).next(error));
+        this.server.on("listening", () => (this.listening$ as Subject<void>).next());
+        this.server.on("close", () => (this.close$ as Subject<void>).next());
+        this.server.on("error", (_: WebSocket, error: Error) => (this.error$ as Subject<any>).next(error));
         setInterval(this.sendRaw.bind(this, this.serialize(0, 0)), 50_000);
-        return merge(this.listening$, this.close$, this.error$).pipe(take(1)).toPromise();
+        return firstValueFrom(merge(this.listening$, this.close$, this.error$));
     }
 
     close() {
@@ -76,10 +73,7 @@ export class RXSocketServer implements RXSocket {
     }
 
     protected wrapSocket(socket: WebSocket, request: IncomingMessage) {
-        return new RXServerSocketClient(
-            socket,
-            request
-        );
+        return new RXServerSocketClient(socket, request);
     }
 
     protected serialize(id: number, event: EventName, data?: any) {
@@ -99,7 +93,7 @@ export class RXSocketServer implements RXSocket {
 
     public sendForResult<I, O>(event: EventName, data: any, socket?: RXSocket) {
         if (!socket) {
-            throw new Error('Destination socket must be specified');
+            throw new Error("Destination socket must be specified");
         }
         return socket.sendForResult<I, O>(event, data);
     }
@@ -114,5 +108,4 @@ export class RXSocketServer implements RXSocket {
         socket.event(name).toggleRemoteSub(socket, subscribing);
         this.event(name).toggleRemoteSub(socket, subscribing);
     }
-
 }
